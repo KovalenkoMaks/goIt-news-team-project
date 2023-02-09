f3b498466566ab4235a62aabfdebdbf269b28a72
SergeyDichko <hillsmen94@gmail.com>
2023-02-09 21:40:36 +0200

    add dark mode

--- src/js/dark_mode/index.js
+++ src/js/dark_mode/index.js
@@ -1,1 +1,27 @@
+   document.querySelector('.ligth').addEventListener('click',() =>{
+
+    if (localStorage.getItem('theme') === 'dark') {
+    localStorage.removeItem('theme');
+    }
+    
+    else{
+        localStorage.setItem('theme', 'dark')
+    }
+    
+    addDarkClassToBody()
+    });
+    
+    function addDarkClassToBody() {
+        try {
+            if (localStorage.getItem('theme') === 'dark'){
+            document.querySelector('body').classList.add('dark');
+        } 
+        else{
+            document.querySelector('body').classList.remove('dark');
+        }
+     } catch (err) { }
+    }
+    addDarkClassToBody ();
+
+    
     
