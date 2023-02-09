1f5d778976bd80fac2f6659bd9f7ec9f55c25c5f
campot05 <daniil.spectr@gmail.com>
2023-02-09 21:40:21 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import './js/popular_render';
-// import { getSearchArticle } from './js/api';
-// getSearchArticle();
-import './js/weather';
+import './js/popular_render';
+import { getSearchArticle } from './js/api';
+getSearchArticle();
