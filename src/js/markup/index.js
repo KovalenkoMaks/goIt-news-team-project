214a9903c43a9d8c71805b4352c330655282efc4
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-02-09 20:31:33 +0200

    Update index.js

--- src/js/markup/index.js
+++ src/js/markup/index.js
@@ -13,7 +13,14 @@ async function getPopularMarkup(arr, number) {
                           alt="">
                       <p class="item-news__category">${elem.section}</p>
                       <button type="button" class="item-news__add-to-favorite">Add to favorite
-                          <svg class="item-news__block-icon" width="16" height="14" ><use href="./images/sprite.svg#heart" class="item-news__icon" ></use></svg>
+                         <svg
+  width="16"
+  height="16"
+  viewBox="0 0 37 32"
+>
+<path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
+
+</svg>
                       </button>
                   </div>
                   <div class="item-news__wrapper-text">
