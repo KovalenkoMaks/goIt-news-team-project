164babcc0b6073a4a33b65a11e5afee041b3e0b9
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-10 20:09:26 +0200

    opacity article

--- src/js/listener/index.js
+++ src/js/listener/index.js
@@ -1,9 +1,19 @@
 import { newsList } from '../refs';
 
 newsList.addEventListener('click', btnAddToFavorite);
+newsList.addEventListener('click', linkReadMore);
 
 function btnAddToFavorite(event) {
   const btn = event.target.closest(`.item-news__add-to-favorite`);
   if (!btn) return;
   btn.classList.toggle('hidden-span');
+}
+
+function linkReadMore(event) {
+  const readMore = event.target.closest(`.item-news__info-link`);
+  if (!readMore) return;
+  readMore.parentNode.parentNode.classList.add('opacity');
+  //   const btn = readMore.parentNode.parentNode.firstElementChild.lastElementChild;
+  //   console.log(btn);
+  //   btn.classList.add('no-opacity');
 }
