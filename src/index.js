a111ce8103b7ab456d48c508e2bc209c78f7bef3
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-02-10 08:18:09 +0100

    add mobile version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './js/popular_render';
+import './js/news_filter/fillter-news-buttons';
--- src/js/news_filter/fillter-news-buttons.js
+++ src/js/news_filter/fillter-news-buttons.js
@@ -1,3 +1,32 @@
 import { getCategoryList } from '../api/index';
 
-console.log(getCategoryList);
+const refs = {
+    categoryContainerEl: document.querySelector('.filter-category__container'),
+}
+
+getCategoryList().then(categoryList => renderMarkupCategory(categoryList))
+
+function renderMarkupCategory(categoryList) {
+    const listEl = document.createElement('ul');
+    listEl.classList.add('filter-category__list');
+    // Разметка:
+    /* <ul class="filter-category__list"></ul> */
+    categoryList.map(({ display_name }) => createMarkupCategory(display_name, listEl));
+    refs.categoryContainerEl.append(listEl);
+}
+function createMarkupCategory (category, listEl) {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('filter-category__item');
+
+    const buttonEl = document.createElement('button');
+    buttonEl.classList.add('filter-category__button');
+    buttonEl.textContent = category;
+    itemEl.append(buttonEl);
+
+    listEl.append(itemEl);
+
+    // Разметка:
+    // <li class="filter-category__item">
+    //     <button class="filter-category__button">{ Category name}</button>
+    // </li>
+ }
