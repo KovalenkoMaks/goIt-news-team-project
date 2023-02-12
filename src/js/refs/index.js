cdd925609f7534697035bcd3621c482653ca1f2a
OlhaMal <117120298+OlhaMal@users.noreply.github.com>
2023-02-12 05:40:57 +0200

    make search input and positioned theme on mobile version

--- src/js/refs/index.js
+++ src/js/refs/index.js
@@ -2,4 +2,5 @@ const newsList = document.querySelector('.list-news');
 const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const closeMenuBtn = document.querySelector('.js-close-menu');
-export { newsList, mobileMenu, openMenuBtn, closeMenuBtn };
+const themeContainer = document.querySelector('.theme');
+export { newsList, mobileMenu, openMenuBtn, closeMenuBtn, themeContainer };
