30074b4c2ab255715149eeb23dc0d158a5cf344e
campot05 <daniil.spectr@gmail.com>
2023-02-12 23:18:49 +0200

    add style in favorite html

--- src/favorite.js
+++ src/favorite.js
@@ -32,14 +32,14 @@ function createMarkup(arr) {
 					  src="${elem.img}"
 					  alt="">
 				 <p class="item-news__category">${elem.category}</p>
-				 <button type="button" class="item-news__add-to-favorite">
-							<span class="item-news__add-to-favorite-btn">Remove from favorite
+				 <button type="button" class="item-news__add-to-favorite hidden-span">
+							<span class="item-news__remove-to-favorite-btn">Remove from favorite
 								<svg class="item-news__block-icon active-news-icon"
 								  width="16"
 								  height="16"
 								  viewBox="0 0 37 32"
 								  >
-						  <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
+								  <path style="stroke: var(--color1, #4440f7)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.2857" d="M10.666 2.286c-4.207 0-7.619 3.377-7.619 7.543 0 3.363 1.333 11.345 14.458 19.413 0.235 0.143 0.505 0.219 0.78 0.219s0.545-0.076 0.78-0.219c13.125-8.069 14.458-16.050 14.458-19.413 0-4.166-3.412-7.543-7.619-7.543s-7.619 4.571-7.619 4.571-3.412-4.571-7.619-4.571z"></path>
 						  </svg></span>
 				  </button>
 			</div>
